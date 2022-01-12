dd4c6517ccffeed859f4dab72f90ae2809b089fc
ivan <ivanbionic@gmail.com>
2022-01-11 16:45:28 -0800

    added some js functions. don't really know how to make the new fetch change the date.

--- nasa-apod/app.js
+++ nasa-apod/app.js
@@ -1,9 +1,13 @@
 const url='https://api.nasa.gov/planetary/apod?api_key=';
 const api_key = config.NASA_API_KEY;
+const dateInput = document.querySelector("#date-picker");
+const currentDate = new Date().toISOString().slice(0,10);  
+
+let newDate = "&date="+dateInput.value+"&";
 
 const fetchNASAData = async () => {
     try{
-        const response = await fetch (`${url}${api_key}`)
+        const response = await fetch (`${url}${api_key}${newDate}`)
         const data = await response.json()
         console.log('NASA APOD data', data)
         displayData(data)
@@ -13,11 +17,31 @@ const fetchNASAData = async () => {
 }
 
 const displayData = data => {
-    document.getElementById('title').textContent = data.title
-    document.getElementById('date').textContent = data.date
-    document.getElementById('picture').src = data.hdurl
-    document.getElementById('explanation').textContent = data.explanation
+    document.getElementById('title').textContent = data.title;
+    document.getElementById('date').textContent = data.date;
+    document.getElementById('picture').src = data.hdurl;
+    document.getElementById('explanation').textContent = data.explanation;
+    
+
+
+    dateInput.max=currentDate;
+    dateInput.min="1995-06-16"; 
+    
+    if (data.hasOwnProperty("copyright")){
+        copyright.innerHTML=data.copyright;
+    } else{
+        copyright.innerHTML=""
+    }
+
+
+ 
   }
 
 fetchNASAData()
+
+
+dateInput.addEventListener('change', (e)=>{
+    e.preventDefault();
+    fetchNASAData();
+})
 
